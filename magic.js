5d3f3a2c4f45351a91afdef88557524dd8bcabf1
Jens Shumway <jensshum@gmail.com>
2022-06-10 22:13:49 -0600

    added javascript, questions section

---
+++ magic.js
@@ -0,0 +1,22 @@
+function question1() {
+    const query = document.querySelector(".why-col")
+    query.innerHTML = "<p> It's gonna be about 1 million bucks</p>"
+}
+function question2() {
+    const query = document.querySelector(".why-col")
+    query.innerHTML = "<p> Just gotta cough up the dough </p>"
+    document.querySelector(".why-zilco table").classList.add("why-zilco-table-shadow")
+}
+function question3() {
+    const query = document.querySelector(".why-col")
+    query.innerHTML = "<p> Nothin. Diddly squat </p>"
+}
+function question4() {
+    const query = document.querySelector(".why-col")
+    query.innerHTML = "<p> Razzamafoo </p>"
+}
+function question5() {
+    const query = document.querySelector(".why-col")
+    query.innerHTML = "<p> And tigger </p>"
+    
+}
