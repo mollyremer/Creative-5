51c4cea73d1faa2d1c7755402135d16aaf1b6f40
Jens Shumway <jensshum@gmail.com>
2022-06-13 17:14:07 -0600

    added responsive fucntionality, other

--- magic.js
+++ magic.js
@@ -1,22 +1,22 @@
 function question1() {
     const query = document.querySelector(".why-col")
-    query.innerHTML = "<p> It's gonna be about 1 million bucks</p>"
+    query.innerHTML = "<p>Just send us an email with what your <br> business needs to automate. <br> We'll reach out to you!</p>"
 }
 function question2() {
     const query = document.querySelector(".why-col")
-    query.innerHTML = "<p> Just gotta cough up the dough </p>"
+    query.innerHTML = "<p> No downloads required. <br> Everything is done in the cloud! </p>"
     document.querySelector(".why-zilco table").classList.add("why-zilco-table-shadow")
 }
 function question3() {
     const query = document.querySelector(".why-col")
-    query.innerHTML = "<p> Nothin. Diddly squat </p>"
+    query.innerHTML = "<p> Support is available <br>around the clock. 24/7.</p>"
 }
 function question4() {
     const query = document.querySelector(".why-col")
-    query.innerHTML = "<p> Razzamafoo </p>"
+    query.innerHTML = "<p> Your data will be 100% secure. <br> Data protection is our <br> #1 priority. </p>"
 }
 function question5() {
     const query = document.querySelector(".why-col")
-    query.innerHTML = "<p> And tigger </p>"
+    query.innerHTML = "<p> All custom-written automation <br> scripts will be available <br> within 1 week of initial estimate. </p>"
     
 }
